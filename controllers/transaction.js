bd50ab3bf60f215983cc10bef6e410d0872a6b0c
AbdullahDashti1 <abdullamustafadashti.0@gmail.com>
2025-11-12 18:30:37 +0300

    update transaction function completed

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -39,3 +39,37 @@ router.post('/', async (req, res) => {
 });
 
 module.exports = router;
+
+// Update - Edit a transaction
+
+router.put('/:transactionId', async (req, res) => {
+  try {
+    const { type, description, amount, transactionDate } = req.body;
+
+    const transaction = await Transaction.findById(req.params.transactionId);
+
+    if (!transaction) {
+      console.log("Transaction not found");
+      return res.status(404).json({ error: 'Transaction not found' });
+    }
+
+    if (!transaction.owner.equals(req.user._id)) {
+      console.log("Permission denied");
+      return res.status(403).json({ error: 'Permission denied' });
+    }
+
+    console.log("Permission granted - updating the transaction");
+
+    const updatedTransaction = await Transaction.findByIdAndUpdate(
+      req.params.transactionId,
+      { type, description, amount, transactionDate },
+      { new: true }
+    );
+
+    console.log("Updated");
+    res.json(updatedTransaction);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to update transaction' });
+  }
+});
